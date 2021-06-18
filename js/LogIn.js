2479806f48fea02e3fae7afaf4bf7e520d464eae
SinkLineP <popovartem505@gmail.com>
2021-06-18 11:05:10 +0300

    add LogIn and Hide btn

--- js/LogIn.js
+++ js/LogIn.js
@@ -1,39 +1,44 @@
+import constants from "./constants.js";
+const { BASE_URL } = constants;
+
 
 export default class LogIn {
    constructor() {
-      this.form = this._createAddPostForm();
+      this.form = this._createAddPostForm()
    }
 
-   createUser() {
-      
-   }
-
-   request(reqType, body) {}
+   request(reqType, body) { }
    _createAddPostForm() {
       const formTemplate = document.getElementById("form-template");
       const addPostForm = formTemplate.content.children["login-form"].cloneNode(true);
-
+      console.log(addPostForm);
       const submit = Array.from(addPostForm).filter((el) => {
-         console.log(el.type === "submit");
-         // return el.type === "submit";
+         axios.get("http://localhost:3000/users")
+         .then(res => res)
+         .then(com => { com
+            com.data.forEach(e => {
+               
+            });
+         })
+         return el.type === "submit";
       })[0];
-
       addPostForm.classList.add("col-12", "mx-auto", "my-5");
-
-      addPostForm.addEventListener("submit", () => {});
-
+      addPostForm.addEventListener("submit", () => { 
+         // const btnEnter = document.getElementById("ModalLogin")
+         // btnEnter.classList.add("hide-btn")
+         // alert("hello")
+      });
       const container = document.createElement("div");
       container.classList.add("container");
-
       const row = document.createElement("div");
       row.classList.add("row");
-
       row.append(addPostForm);
       container.append(row);
-
       return container;
    }
+
    _createControlBtn(operationType, content) {
       return "button";
    }
 }
+
--- js/index.js
+++ js/index.js
@@ -6,18 +6,54 @@ const visit = new Visit();
 const login = new LogIn();
 const modal = new Modal();
 
-
 const root = document.getElementById("root");
 const loginModal = document.getElementById("ModalLogin");
-const visitModal = document.getElementById("ModalVisit")
+const visitModal = document.getElementById("ModalVisit");
+const btnVisit = document.getElementById("btn-visit");
+
 
 loginModal.addEventListener("click", () => {
    root.append(modal.render(login.form));
+   const emailLog = document.getElementById("email-login");
+   const passwordLog = document.getElementById("userPassword");
+   const btnLogin = document.getElementById("btn-login-submit");
+   const formTemplateLogin = document.getElementById("form-template");
+   const addLoginForm = formTemplateLogin.content.children["login-form"].cloneNode(true);
+   const ModalClose = document.getElementById("modal-close");
+
+   btnLogin.addEventListener("click", () => {
+      const emailLogValue = emailLog.value;
+      const passwordLogValue = passwordLog.value;
+
+      Array.from(addLoginForm).filter((el) => {
+         axios.get("http://localhost:3000/users")
+            .then(res => res)
+            .then(com => {
+               com.data.forEach(e => {
+                  if (emailLogValue == e.email && passwordLogValue == e.password) {
+                     console.log("Done")
+                     loginModal.style.display = "none";
+                     btnVisit.classList.remove("hide-btn")
+                     ModalClose.click();
+                  } else {
+                     console.log("Error")
+                  };  
+               });
+            })
+         return el.type === "submit";
+      })[0];
+     
+   })
+
    modal.show();
 });
 
+
+
 visitModal.addEventListener("click", () => {
-   root.append(modal.render(visit.form))
-   modal.show()
+   root.append(modal.render(visit.form));
+   modal.show();
 })
+
+
 
